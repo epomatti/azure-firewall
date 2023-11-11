6e9682875daf5cbe16f4ce8eb92b4e9de7929c38
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-11-11 18:43:56 -0300

    application rule

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -61,25 +61,25 @@ resource "azurerm_firewall_policy_rule_collection_group" "collection_group_terra
   firewall_policy_id = azurerm_firewall_policy.policy_01.id
   priority           = 500
 
-  # application_rule_collection {
-  #   name     = "app_rule_collection1"
-  #   priority = 500
-  #   action   = "Allow"
+  application_rule_collection {
+    name     = "HTTP"
+    priority = 500
+    action   = "Allow"
 
-  #   rule {
-  #     name = "app_rule_collection1_rule1"
-  #     protocols {
-  #       type = "Http"
-  #       port = 80
-  #     }
-  #     protocols {
-  #       type = "Https"
-  #       port = 443
-  #     }
-  #     source_addresses  = ["10.0.0.1"]
-  #     destination_fqdns = ["*.microsoft.com"]
-  #   }
-  # }
+    rule {
+      name = "Microsoft"
+      protocols {
+        type = "Http"
+        port = 80
+      }
+      protocols {
+        type = "Https"
+        port = 443
+      }
+      source_ip_groups  = [var.vnet_ip_group_id]
+      destination_fqdns = ["*.microsoft.com"]
+    }
+  }
 
   network_rule_collection {
     name     = "AllowVNets"
